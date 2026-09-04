ae9d7c93ddf443d83f0d0d98c574d8b95774ebd2
amogelangntokozo05-ui <amogelangntokozo05@gmail.com>
2026-09-04 02:37:42 +0200

    Add constructor and getters/setters for user details in Login class

--- src/main/java/com/mycompany/login/Login.java
+++ src/main/java/com/mycompany/login/Login.java
@@ -44,3 +44,55 @@ public class Login {
       this.password = "";
       this.cellPhoneNumber = "";
    }
+
+   public Login(String firstName, String lastName, String username, String password, String cellPhoneNumber) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.username = username;
+      this.password = password;
+      this.cellPhoneNumber = cellPhoneNumber;
+   }
+
+   // --- Getters and Setters ---
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   public String getUsername() {
+      return username;
+   }
+
+   public void setUsername(String username) {
+      this.username = username;
+   }
+
+   public String getPassword() {
+      return password;
+   }
+
+   public void setPassword(String password) {
+      this.password = password;
+   }
+
+   public String getCellPhoneNumber() {
+      return cellPhoneNumber;
+   }
+
+   public void setCellPhoneNumber(String cellPhoneNumber) {
+      this.cellPhoneNumber = cellPhoneNumber;
+   }
+
+   
